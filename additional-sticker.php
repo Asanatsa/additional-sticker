8a213c005b6766704610e5592a06a762ac2c27d5
Asanatsa <me@asanatsa.cc>
2025-07-27 03:15:03 +0800

    update ver.

--- additional-sticker.php
+++ additional-sticker.php
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.1.1 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'ADDITIONAL_STICKER_VERSION', '1.1.1' );
+define( 'ADDITIONAL_STICKER_VERSION', '2.0.0' );
 
 /**
  * The code that runs during plugin activation.
